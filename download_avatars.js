67fde5e3042046158d174c865700359435c4f75a
ravindra rawat <ravinrawat.wd@gmail.com>
2018-01-23 01:32:40 +0000

    step 6 completed

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,20 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/jquery/${repoName}`,
+    headers: {
+      'User-agent': 'request',
+    }
+
+  }
+
+  var url = `https://api.github.com/repos/${repoOwner}/jquery/${repoName}`;
+  request(url ,function(err, res, body){
+    cb(err, body);
+  })
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
